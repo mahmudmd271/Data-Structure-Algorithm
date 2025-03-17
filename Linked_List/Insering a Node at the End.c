c056831d159b8171c60df27a9258a05319c00b0a
Md Mahmud Hossain <158801233+mahmudmd271@users.noreply.github.com>
2025-03-17 16:42:31 +0600

    Add files via upload

---
+++ Linked_List/Insering a Node at the End.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+void add_at_end(struct node *head, int data)
+{
+    struct node *ptr, *temp;
+    ptr = head;
+    temp = (struct node*)malloc(sizeof(struct node));
+
+    temp->data = data;
+    temp->link = NULL;
+    while(ptr->link!= NULL)
+    {
+        ptr = ptr->link;
+    }
+    ptr->link = temp;
+}
+
+int main()
+{
+
+
+    struct node *head = malloc(sizeof(struct node));
+    head->data = 45;
+    head->link = NULL;
+
+    struct node *current = malloc(sizeof(struct node));
+    current->data = 98;
+    current->link = NULL;
+    head->link = current;
+
+    current = malloc(sizeof(struct node));
+    current->data = 3;
+    current->link = NULL;
+
+    head->link->link = current;
+
+    add_at_end(head,67);
+
+    return 0;
+}
